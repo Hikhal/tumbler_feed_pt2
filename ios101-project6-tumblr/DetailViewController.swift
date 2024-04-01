6548f0043e7ce63ea04de6d0e500994ec7fc80cc
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2024-03-31 23:55:55 -0400

    Initial commit

---
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ios101-project6-tumblr
+//
+//  Created by Hamza Khaliq on 3/31/24.
+//
+
+import UIKit
+import Nuke
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var summaryView: UITextView!
+    @IBOutlet weak var pictureView: UIImageView!
+    var post: Post!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            title = "Details"
+            summaryView.isEditable = false;
+            summaryView.isScrollEnabled = true;
+            summaryView.text = post.caption.trimHTMLTags()
+       
+       
+    }
+        
+       
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
